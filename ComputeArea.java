12c865643648ef64ba6a344f785883c0dd54981d
Amjad ali <amjadbhuttof@gmail.com>
2026-02-16 16:27:20 +0500

    Add files via upload

---
+++ ComputeArea.java
@@ -0,0 +1,10 @@
+public class ComputeArea {
+    public static void main(String[] args) {
+        float radius = 3.5f;
+        double area;
+
+        area =radius*radius*3.14;
+        System.out.println("Area of circle is : "+area);
+  
+    }
+}
