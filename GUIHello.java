413c14e99cbe5b90d132ccbcf291bbda3c9d0640
Amjad ali <amjadbhuttof@gmail.com>
2026-02-26 20:54:01 +0500

    Add files via upload

---
+++ GUIHello.java
@@ -0,0 +1,6 @@
+///import javax.swing.JOptionPane;
+public class GUIHello {
+    public static void main(String[] args) {
+         javax.swing.JOptionPane.showMessageDialog(null,"Hello world");
+    }
+}
